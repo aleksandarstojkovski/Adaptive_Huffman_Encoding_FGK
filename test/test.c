463a45aa6a4a5cd2bd557a1a45b1dec21c27c64d
code-mds <43860962+code-mds@users.noreply.github.com>
2018-10-22 20:15:10 +0200

    test bit manipulation

--- test/test.c
+++ test/test.c
@@ -10,6 +10,13 @@
 int testReadAllBinaryFiles();
 int testReadBinaryFile(const char *filename);
 
+void test_bit();
+void test_bit_check(char toTest, int bit_pos, char expected);
+void test_bit_set_zero(char toTest, int bit_pos, char expected);
+void test_bit_set_one(char toTest, int bit_pos, char expected);
+void test_bit_copy(char byte_from, char byte_to, int read_pos, int write_pos, int size, char expected);
+
+
 static const char * TEST_FILES[NUM_TEST_FILES] = {
         "../../test-res/ff_ff_ff",
         "../../test-res/alice.txt",
@@ -40,11 +47,7 @@ int testReadBinaryFile(const char *filename) {
     trace("Start testReadBinaryFile: [%s]\n", filename);
 
     int rc = readBinaryFile(filename, traceCharBin);
-
     trace("\nRead completed, return code: %d\n", rc);
-    trace("press a key to continue\n");
-    getchar();
-
     return rc;
 }
 
@@ -52,5 +55,54 @@ int testReadBinaryFile(const char *filename) {
  * Main function
  */
 int main(int argc, char* argv[]) {
+    test_bit();
+
     testReadAllBinaryFiles();
+}
+
+void test_bit() {
+    // 2 = 00000010
+    test_bit_check(2, 2, BIT_0);
+    test_bit_check(2, 1, BIT_1);
+    test_bit_check(2, 0, BIT_0);
+
+    // 8 = 00001000
+    test_bit_check(8, 4, BIT_0);
+    test_bit_check(8, 3, BIT_1);
+    test_bit_check(8, 2, BIT_0);
+
+    // 8 = 00001000
+    test_bit_set_one(8, 0, BIT_1);
+    test_bit_set_one(8, 7, BIT_1);
+
+    // 9 = 00001001
+    test_bit_set_zero(9, 0, BIT_0);
+    test_bit_set_zero(9, 1, BIT_0);
+
+    // source 2 = 00000010
+    // dest   1 = 00000001
+    // res    9 = 00001001
+    test_bit_copy(2, 1, 0, 2, 2, 9);
+}
+
+void test_bit_set_one(char toTest, int bit_pos, char expected) {
+    bit_set_one(&toTest, bit_pos);
+    test_bit_check(toTest, bit_pos, expected);
+}
+
+void test_bit_set_zero(char toTest, int bit_pos, char expected) {
+    bit_set_zero(&toTest, bit_pos);
+    test_bit_check(toTest, bit_pos, expected);
+}
+
+void test_bit_check(char toTest, int bit_pos, char expected) {
+    char val = bit_check(toTest, bit_pos);
+    if(val != expected)
+        perror("error checking bit");
+}
+
+void test_bit_copy(char byte_from, char byte_to, int read_pos, int write_pos, int size, char expected) {
+    bit_copy(&byte_to, byte_from, read_pos, write_pos, size);
+    if(byte_to != expected)
+        perror("error copying bits");
 }
